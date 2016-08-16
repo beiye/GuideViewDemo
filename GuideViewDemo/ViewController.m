05ececec26cfb14ec05d27384f5b111f2363bcac
peter.shi <532289386@qq.com>
2016-08-16 13:52:45 +0800

    镂空效果

--- GuideViewDemo/ViewController.m
+++ GuideViewDemo/ViewController.m
@@ -10,18 +10,33 @@
 
 @interface ViewController ()
 
+@property (nonatomic, weak) IBOutlet UIButton *button1;
+@property (nonatomic, weak) IBOutlet UIButton *button2;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIView *backgroundView = [[UIView alloc] initWithFrame:self.view.frame];
+    backgroundView.backgroundColor = [UIColor colorWithWhite:0.0 alpha:0.6];
+    
+    UIBezierPath *path = [UIBezierPath bezierPathWithRect:self.view.frame];
+    UIBezierPath *circlePath1 = [UIBezierPath bezierPathWithArcCenter:self.button1.center radius:(self.button1.frame.size.height/2 + 5) startAngle:0.0 endAngle:2*M_PI clockwise:NO];
+    UIBezierPath *circlePath2 = [UIBezierPath bezierPathWithArcCenter:self.button2.center radius:(self.button2.frame.size.width/2 + 5) startAngle:0.0 endAngle:2*M_PI clockwise:NO];
+    
+    [path appendPath:circlePath1];
+    [path appendPath:circlePath2];
+    
+    CAShapeLayer *layer = [CAShapeLayer layer];
+    layer.path = path.CGPath;
+    
+    backgroundView.layer.mask = layer;
+    
+    [self.view addSubview:backgroundView];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
